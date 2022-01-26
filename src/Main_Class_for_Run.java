d61f0a1808c0a1b8cc00afa9a0287e937dad0b3e
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-01-27 01:29:25 +0600

    thes all code till now, for variable (whith out method)

--- src/Main_Class_for_Run.java
+++ src/Main_Class_for_Run.java
@@ -15,9 +15,28 @@ public class Main_Class_for_Run {
       System.out.println("Email: "+student_1.email);
       System.out.println("Address: "+student_1.address);
       System.out.println("Phone: "+student_1.phn);
+      System.out.println("----------------");
 
       Student student_2;
       student_2 = new Student();
+
+      student_2.name= "Kawsar(buira)";
+      student_2.age=57;
+      student_2.email="abdullah15-14325@diu.edu.bd";
+      student_2.address="Dhaka";
+      student_2.phn=179700025;
+
+      System.out.println("Name: "+student_2.name);
+      System.out.println("Age: "+student_2.age);
+      System.out.println("Email: "+student_2.email);
+      System.out.println("Address: "+student_2.address);
+      System.out.println("Phone: "+student_2.phn);
+      System.out.println("----------------");
+
+
+
+
+
 
    }
 

c30abf6dd7d85edacc2934e44b9f2774d5e0cc38
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-01-27 02:11:34 +0600

    Variable+Method

---
+++ src/Main2.java
@@ -0,0 +1,25 @@
+public class Main2 {
+
+    public static void main(String[] args){
+
+        Student_2 student_1 = new Student_2();
+
+        student_1.name= "mahamud";
+        student_1.age=20;
+        student_1.email="mahamud-15-13756@diu.edu.bd";
+        student_1.address="Dhaka";
+        student_1.phn=1744385565;
+        student_1.Display();
+
+
+        Student_2 student_2;
+        student_2=new Student_2();
+        student_2.name= "Kawsar(buira)";
+        student_2.age=57;
+        student_2.email="abdullah15-14325@diu.edu.bd";
+        student_2.address="Dhaka";
+        student_2.phn=179700025;
+        student_2.Display();
+
+    }
+}
---
+++ src/Student_2.java
@@ -0,0 +1,19 @@
+public class Student_2 {
+
+    public String name;
+    public String email;
+    public String address;
+    public int age;
+    public int phn;
+
+    void Display(){
+
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Email: "+email);
+        System.out.println("Address: "+address);
+        System.out.println("Phone: "+phn);
+        System.out.println("----------------");
+
+    }
+}

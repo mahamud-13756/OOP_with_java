b85a1a2b8429124df93b30f89b97456f1625e299
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-01-27 00:30:06 +0600

    Class and Object code

---
+++ src/Main_Class_for_Run.java
@@ -0,0 +1,21 @@
+public class Main_Class_for_Run {
+
+   public static void main(String[] args){
+
+      Student student_1 = new Student();
+
+      student_1.name= "mahamud";
+      student_1.age=20;
+      student_1.email="mahamud-15-13756@diu.edu.bd";
+      student_1.address="Dhaka";
+      student_1.phn=1744385565;
+
+      System.out.println("Name: "+student_1.name);
+      System.out.println("Age: "+student_1.age);
+      System.out.println("Email: "+student_1.email);
+      System.out.println("Address: "+student_1.address);
+      System.out.println("Phone: "+student_1.phn);
+   }
+
+
+}
---
+++ src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+
+    public String name;
+    public String email;
+    public String address;
+    public int age;
+    public int phn;
+
+}

2080dd4be126dea35da647cbd8858ac67352d81e
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-01-27 03:41:50 +0600

    parameter one type

---
+++ src/Main3.java
@@ -0,0 +1,17 @@
+public class Main3 {
+
+    public static void main(String[] args){
+
+        Student_3 student_1 = new Student_3();
+
+        student_1.Display("mahamud",20,"dhaka");
+
+
+        Student_3 student_2;
+        student_2=new Student_3();
+
+        student_2.Display("Kawsar(buira)",57,"dhaka");
+
+
+
+}}
---
+++ src/Student_3.java
@@ -0,0 +1,13 @@
+public class Student_3 {
+
+    public String name;
+    public String address;
+    public int age;
+
+    void Display(String name,int age,String address){
+
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Address: "+address);
+        System.out.println("----------------");
+}}

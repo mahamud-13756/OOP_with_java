7bb4fcec023dab0d6619a2f47a73ca80b212aa6c
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-01-28 02:04:03 +0600

    constructor in an one file

---
+++ src/Constructor/Teacher1.java
@@ -0,0 +1,21 @@
+package Constructor;
+
+public class Teacher1 {
+    String Name;
+    int id;
+    String dept;
+    String sub;
+
+    Teacher1(){
+
+        System.out.println("I am a teacher.");
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+    Teacher1 ob1=new Teacher1();
+    }
+}

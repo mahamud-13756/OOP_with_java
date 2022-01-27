03c45e9edfe2e1a8e81a77c7a7fa09efd81eb3fa
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-01-28 02:17:49 +0600

    parameterised Constructor in an one file

--- src/Constructor/Teacher1.java
+++ src/Constructor/Teacher1.java
@@ -7,15 +7,25 @@ public class Teacher1 {
     String sub;
 
     Teacher1(){
-
         System.out.println("I am a teacher.");
-
-
-
+        System.out.println("--------------");
     }
-
+    Teacher1(String name,int id,String dept,String sub){
+        System.out.println("Name: "+name);
+        System.out.println("ID: "+id);
+        System.out.println("Dept: "+dept);
+        System.out.println("Sub: "+sub);
+        System.out.println("--------------");
+    }
+    Teacher1(String name,int id) {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("--------------");
+    }
     public static void main(String[] args) {
 
     Teacher1 ob1=new Teacher1();
+    Teacher1 ob2=new Teacher1("Mahamud",101,"CSE","Algorith");
+    Teacher1 ob3=new Teacher1("Najib",102);
     }
 }

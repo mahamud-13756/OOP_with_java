047bfbda21e866aa5a77ea3cf8ca5f5f459a50e9
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-01-29 00:44:52 +0600

    main solution

--- src/Solution/Box.java
+++ src/Solution/Box.java
@@ -1,38 +1,21 @@
 package Solution;
 
-import java.util.Scanner;
-
 public class Box {
-//    Scanner ob1 = new Scanner(System.in);
-//    int height1= ob1.nextInt();
-//    int width1= ob1.nextInt();
-//    int depth1= ob1.nextInt();
-//
-//    int height2= ob1.nextInt();
-//    int width2= ob1.nextInt();
-//    int depth2= ob1.nextInt();
 
-    public static void main(String[] args) {
-        Scanner ob1 = new Scanner(System.in);
-        int height1= ob1.nextInt();
-        int width1= ob1.nextInt();
-        int depth1= ob1.nextInt();
+    double height;
+    double width;
+    double depth;
+    Box(double h,double w,double d){
 
-        int height2= ob1.nextInt();
-        int width2= ob1.nextInt();
-        int depth2= ob1.nextInt();
-
-        System.out.println("you entered: "+height1);
-        System.out.println("you entered: "+width1);
-        System.out.println("you entered: "+depth1);
-        System.out.println("you entered: "+height2);
-        System.out.println("you entered: "+width2);
-        System.out.println("you entered: "+depth2);
+        height=h;
+        width=w;
+        depth=d;
 
     }
+    void displayVol(){
+        double vol=height*width*depth;
 
-
-
-
+        System.out.println("Volume of 1st Box: "+vol);
+    }
 
 }

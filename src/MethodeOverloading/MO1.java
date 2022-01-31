ea489c6574f8d6f701c1644b9f2570bf075a9d79
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-01 02:09:36 +0600

    This is a method overloading code. and specially this commit is for 1st method and print nothing to add

---
+++ src/MethodeOverloading/MO1.java
@@ -0,0 +1,9 @@
+package MethodeOverloading;
+
+public class MO1 {
+    public static void main(String[] args) {
+        M_overload ob1= new M_overload();
+        ob1.add();
+    }
+
+}
---
+++ src/MethodeOverloading/M_overload.java
@@ -0,0 +1,20 @@
+package MethodeOverloading;
+
+public class M_overload {
+
+    void add(int a, int b){
+        System.out.println(a+b);
+    }
+    void add(double a, double b){
+        System.out.println(a+b);
+    }
+    void add(double a, int b){
+        System.out.println(a+b);
+    }
+    void add(){
+        System.out.println("Nothing to add");
+    }
+
+
+
+}

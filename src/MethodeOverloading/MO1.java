0a21be678a000bb2ed110d5464cf18df3b19ac86
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-01 02:14:26 +0600

    all properties are worked

--- src/MethodeOverloading/MO1.java
+++ src/MethodeOverloading/MO1.java
@@ -4,6 +4,9 @@ public class MO1 {
     public static void main(String[] args) {
         M_overload ob1= new M_overload();
         ob1.add();
+        ob1.add(15,15);
+        ob1.add(15.6,16.4);
+        ob1.add(18.8,10);
     }
 
 }

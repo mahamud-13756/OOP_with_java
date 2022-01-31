6b7e25c7ee348410927dd9fe7068248ff38a9c18
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-01-31 23:28:12 +0600

    print power value

--- src/MathDemo.java
+++ src/MathDemo.java
@@ -4,5 +4,6 @@ public class MathDemo {
 
         System.out.println(Math.abs(-5));// this Math.abs print absolute value
         System.out.println(Math.sqrt(49));
+        System.out.println(Math.pow(3,2));
     }
 }

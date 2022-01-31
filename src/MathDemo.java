3c5ad830e37ce3f98a57b7d1f529c4973cd33c83
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-01-31 23:26:39 +0600

    this Math.sqrt print Squreroot value of the number

--- src/MathDemo.java
+++ src/MathDemo.java
@@ -3,5 +3,6 @@ public class MathDemo {
     public static void main(String[] args) {
 
         System.out.println(Math.abs(-5));// this Math.abs print absolute value
+        System.out.println(Math.sqrt(49));
     }
 }

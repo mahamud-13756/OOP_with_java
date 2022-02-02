93bc64383d1de1ae2f12f3400fdbe55ac869357c
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-02 23:53:08 +0600

    Remove comment line

--- src/MethodeOverloading/MO1.java
+++ src/MethodeOverloading/MO1.java
@@ -5,8 +5,7 @@ public class MO1 {
         M_overload ob1= new M_overload();
         ob1.add();
         ob1.add(15,15);
-//        System.out.println("Addition is: "+sum);
-//        ob1.add(15.6,16.4);
+
         ob1.add(18.8,10);
     }
 

67f957a3d94c412ce85631a8ef8ed3cbabde2dfa
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-04 01:31:18 +0600

    Recurtion code example

---
+++ src/Recurtion/FacTest.java
@@ -0,0 +1,9 @@
+package Recurtion;
+
+public class FacTest {
+    public static void main(String[] args) {
+        FactorialDemo ob = new FactorialDemo();
+        int f =ob.fact(5);
+        System.out.println(f);
+    }
+}
---
+++ src/Recurtion/FactorialDemo.java
@@ -0,0 +1,12 @@
+package Recurtion;
+
+public class FactorialDemo {
+    int fact (int n){
+        if(n==0) {
+            return 1;
+        }
+        else {
+            return n*fact(n-1);
+        }
+    }
+}

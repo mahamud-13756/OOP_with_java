6e161deaf015ccad314f78b40812552e950bbea0
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-03 20:44:42 +0600

    this is test program class for check how to works call by value

---
+++ src/CallTest.java
@@ -0,0 +1,14 @@
+public class CallTest {
+    public static void main(String[] args) {
+        int x=10;
+        CallByValue ob1= new CallByValue();
+        System.out.println("The value of x: "+x);
+
+        ob1.check(x);
+        System.out.println("x after call: "+x);
+
+
+    }
+
+
+}

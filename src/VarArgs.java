b4d2417bc99970e7b117aee1866396ed2fcca409
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-04 01:13:26 +0600

    Variable length Arguments

---
+++ src/VarArgs.java
@@ -0,0 +1,10 @@
+public class VarArgs {
+    void add(int ... x){ // variable length arguments
+        int sum=0;
+        for(int i : x){ // for each loop
+            sum=sum+i;
+
+        }
+        System.out.println(sum);
+    }
+}
---
+++ src/VarArgs1.java
@@ -0,0 +1,13 @@
+public class VarArgs1 {
+    public static void main(String[] args) {
+        VarArgs ob= new VarArgs();
+        ob.add(5,5);
+        ob.add(5,5,5);
+        ob.add(5,5,5,5);
+        ob.add(5,5,5,5,5);
+        ob.add(5,5,5,5,5,5);
+    }
+
+
+
+}

cf6c386f98aebc74a9e612709bac94807d47aee8
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-04 01:47:10 +0600

    User input factirial

--- src/Recurtion/FacTest.java
+++ src/Recurtion/FacTest.java
@@ -1,9 +1,18 @@
 package Recurtion;
 
+import java.util.Scanner;
+
 public class FacTest {
     public static void main(String[] args) {
-        FactorialDemo ob = new FactorialDemo();
-        int f =ob.fact(5);
-        System.out.println(f);
+        while(true) {
+            System.out.println("Please enter a value: ");
+
+            Scanner s = new Scanner(System.in);
+            int n = s.nextInt();
+            FactorialDemo ob = new FactorialDemo();
+            int f = ob.fact(n);
+
+            System.out.println("The factorial is: "+f);
+        }
     }
 }

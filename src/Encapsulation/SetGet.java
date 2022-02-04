31dcb20e6b1010495778f5b4a42638faee78a7b9
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-04 20:16:31 +0600

    Just object and class er use ...

---
+++ src/Encapsulation/SetGet.java
@@ -0,0 +1,11 @@
+package Encapsulation;
+
+public class SetGet {
+    String name;
+    int age;
+
+    void display(){
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+    }
+}
---
+++ src/Encapsulation/SetGet1.java
@@ -0,0 +1,10 @@
+package Encapsulation;
+
+public class SetGet1 {
+    public static void main(String[] args) {
+        SetGet ob1=new SetGet();
+        ob1.name="Mahamud";
+        ob1.age=23;
+        ob1.display();
+    }
+}

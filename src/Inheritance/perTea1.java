b1e90b619f3e6939fa64f0977267550e75eb2522
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-04 23:53:26 +0600

    Another way total 4 class

--- src/Inheritance/perTea1.java
+++ src/Inheritance/perTea1.java
@@ -3,21 +3,25 @@ package Inheritance;
 import java.util.Scanner;
 
 public class perTea1 {
-    public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        Teacher ob1=new Teacher();
-        ob1.name=s.nextLine();
-        ob1.age=23;
-        ob1.phn=1627395446;
-        ob1.add="Dhaka";
-        ob1.display1();
-        ob1.display2();
-        Teacher ob2=new Teacher();
-        ob2.name="Aminul";
-        ob2.age=24;
-        ob2.phn=16273;
-        ob2.add="Dhaka";
-        ob2.display1();
-        ob2.display2();
-    }
+    //public static void main(String[] args) {
+
+        void display3(int a,int p,String ad){
+            Scanner s=new Scanner(System.in);
+            Teacher ob1=new Teacher();
+            ob1.name=s.nextLine();
+            ob1.age=23;
+            ob1.phn=1627395446;
+            ob1.add="Dhaka";
+            ob1.display1();
+            ob1.display2();
+        }
+
+//        Teacher ob2=new Teacher();
+//        ob2.name="Aminul";
+//        ob2.age=24;
+//        ob2.phn=16273;
+//        ob2.add="Dhaka";
+//        ob2.display1();
+//        ob2.display2();
+
 }
---
+++ src/Inheritance/perTea2.java
@@ -0,0 +1,9 @@
+package Inheritance;
+
+public class perTea2 {
+    public static void main(String[] args) {
+        perTea1 ob3=new perTea1();
+        ob3.display3(21,1627395555,"Dhaka");
+        ob3.display3(25,16273,"Rajshahi");
+    }
+}

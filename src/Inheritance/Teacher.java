bef99559188546f0cd7131a22b8e5cbba6adce98
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-04 22:39:18 +0600

    inheritance code with user input

---
+++ src/Inheritance/Teacher.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+public class Teacher extends person {
+    String add;
+
+    void display2(){
+
+        System.out.println("Address: "+add);
+
+    }
+
+}
---
+++ src/Inheritance/person.java
@@ -0,0 +1,14 @@
+package Inheritance;
+
+public class person {
+    String name;
+    int age;
+    int phn;
+
+    void display1 (){
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+        System.out.println("Phone:"+phn);
+
+    }
+}

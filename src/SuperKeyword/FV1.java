b4d2a280aee5cf56f9c98a605880bad9077b7a97
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-05 17:03:13 +0600

    now this code is showing 10 for super keyword

--- src/SuperKeyword/FV1.java
+++ src/SuperKeyword/FV1.java
@@ -4,6 +4,7 @@ public class FV1 extends ForVariable {
     //int x=10;
     int x=5;
     void display(){
-        System.out.println("The value is: "+x);//here the value is 5,because local variable is powerful.
+        //System.out.println("The value is: "+x);//here the value is 5,because local variable is powerful.
+        System.out.println("The value is: "+super.x);//now this is print 10 because i use super keyword
     }
 }

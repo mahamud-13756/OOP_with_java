7327ba8ce47101f326ff58a4662c0401e0bf1367
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-07 03:56:09 +0600

    This is run time polymorphism

--- src/Polymorphism/P1Test.java
+++ src/Polymorphism/P1Test.java
@@ -2,11 +2,19 @@ package Polymorphism;
 
 public class P1Test {
     public static void main(String[] args) {
+//        P1Person p=new P1Person();
+//        p.display();
+//        P1Teacher t=new P1Teacher(); // Compile time polymorphism
+//        t.display();
+//        P1Student s=new P1Student();
+//        s.display();
+
         P1Person p=new P1Person();
         p.display();
-        P1Teacher t=new P1Teacher();
-        t.display();
-        P1Student s=new P1Student();
-        s.display();
+        p=new P1Teacher();
+        p.display();
+        p=new P1Student();
+        p.display();
+
     }
 }

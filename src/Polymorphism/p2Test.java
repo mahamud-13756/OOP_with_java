d5722f73949eb618de7dea7c2c2f653bfe8549d1
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-07 04:37:28 +0600

    this is dynamic or run time poly

--- src/Polymorphism/p2Test.java
+++ src/Polymorphism/p2Test.java
@@ -2,13 +2,21 @@ package Polymorphism;
 
 public class p2Test {
     public static void main(String[] args) {
+//        p2Shape s=new p2Shape();
+//        p2Rectangle r=new p2Rectangle(10,20);
+//        p2Triangle t=new p2Triangle(2,4);
+//
+//        System.out.println(s.area());
+//        System.out.println(r.area());
+//        System.out.println(t.area());
+
         p2Shape s=new p2Shape();
-        p2Rectangle r=new p2Rectangle(10,20);
-        p2Triangle t=new p2Triangle(2,4);
+        p2Shape s1=new p2Rectangle(10,20);
+        p2Shape s2=new p2Triangle(2,4);
 
         System.out.println(s.area());
-        System.out.println(r.area());
-        System.out.println(t.area());
+        System.out.println(s1.area());
+        System.out.println(s2.area());
 
     }
 }

61f91ef29d336559d32c0873093b4928028011da
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-07 04:39:20 +0600

    this is another way of run time poly

--- src/Polymorphism/p2Test.java
+++ src/Polymorphism/p2Test.java
@@ -10,13 +10,24 @@ public class p2Test {
 //        System.out.println(r.area());
 //        System.out.println(t.area());
 
-        p2Shape s=new p2Shape();
-        p2Shape s1=new p2Rectangle(10,20);
-        p2Shape s2=new p2Triangle(2,4);
+//        p2Shape s=new p2Shape();
+//        p2Shape s1=new p2Rectangle(10,20);
+//        p2Shape s2=new p2Triangle(2,4);
+//
+//        System.out.println(s.area());
+//        System.out.println(s1.area());
+//        System.out.println(s2.area());
 
+        p2Shape s=new p2Shape();
         System.out.println(s.area());
-        System.out.println(s1.area());
-        System.out.println(s2.area());
+        s=new p2Rectangle(10,20);
+        System.out.println(s.area());
+        s=new p2Triangle(2,4);
+        System.out.println(s.area());
+
+
+
+
 
     }
 }
